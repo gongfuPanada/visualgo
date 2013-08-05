cf74c5d1afb38f5b57eb5485740cd0c08f56c110
Ilariia Belova <ilariyabelova@gmail.com>
2013-08-05 15:08:09 -0700

    First working springmvc application

--- trunk/src/main/java/com/github/jhilary/visualgo/web/VisualgoController.java
+++ trunk/src/main/java/com/github/jhilary/visualgo/web/VisualgoController.java
@@ -1,7 +1,14 @@
 package com.github.jhilary.visualgo.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class VisualgoController {
+public final class VisualgoController {
+    public VisualgoController(){};
+    @RequestMapping
+    public void zero(Model model) {
+      model.addAttribute(0);
+    }
   }
---
+++ trunk/src/main/webapp/WEB-INF/jsp/mincut/algos.jsp
@@ -0,0 +1,16 @@
+<%-- Source project: sip03, branch: 01 (Maven Project) --%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+
+<html>
+<head>
+<title>Visualgo</title>
+</head>
+<body>
+	<h1>Visualgo</h1>
+	<ul> 
+		<li>
+		Zero value: ${integer} 
+		</li>
+	</ul>
+</body>
+</html>
