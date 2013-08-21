240d7b7323f1bc6af497fb81fd6ad3f2825d68d4
Ilariia Belova <ilariyabelova@gmail.com>
2013-08-21 13:55:22 -0700

    Spaces

--- src/main/java/com/github/jhilary/visualgo/graph/Graph.java
+++ src/main/java/com/github/jhilary/visualgo/graph/Graph.java
@@ -6,48 +6,47 @@ import java.util.LinkedList;
 import com.github.jhilary.visualgo.graph.exception.GraphException;
 
 public abstract class Graph{
-	HashMap<Integer, Node> nodes = new HashMap<Integer, Node>();
-	LinkedList<Edge> edges = new LinkedList<Edge>();
-	
-	public Graph() {}
-	
-	public Graph(HashMap<Integer, LinkedList<Integer>> nodeGraph) {};
-		
-	public Graph (Graph g){
-		for (Node node: g.getNodes().values()) {
-			this.addNode(node.getLabel().getValue());
-		}
-		for (Edge edge: g.getEdges()) {
-			Node from = nodes.get(edge.getFirst().getLabel().getValue());
-			Node to = nodes.get(edge.getSecond().getLabel().getValue());
-			this.addEdge(from, to);
-		}
-	}
-	
-	
-	public Graph(HashMap<Integer, Node> nodes, LinkedList<Edge> edges){
-		this.nodes = nodes;
-		this.edges = edges;
-	}
-	
-	public HashMap<Integer, Node> getNodes() {
-		return nodes;
-	}
-	public LinkedList<Edge> getEdges() {
-		return edges;
-	}
-	
-	public abstract void addNode(Integer label);
-//	public abstract void addNode(Label label);
-	
-	public abstract void addEdge(Integer from, Integer to) throws GraphException;
-//	public abstract void addEdge(Label from, Label to) throws GraphException;
-	
-	public abstract void removeEdge(Edge edge);
-	
-	abstract void mergeEdge(Edge edge);
-	abstract void addEdge(Node node1, Node node2);
-	
-	public abstract int minCut() throws GraphException;
+    HashMap<Integer, Node> nodes = new HashMap<Integer, Node>();
+    LinkedList<Edge> edges = new LinkedList<Edge>();
+
+    public Graph() {}
+    
+    public Graph(HashMap<Integer, LinkedList<Integer>> nodeGraph) {};
+
+    public Graph (Graph g){
+        for (Node node: g.getNodes().values()) {
+            this.addNode(node.getLabel().getValue());
+        }
+        for (Edge edge: g.getEdges()) {
+            Node from = nodes.get(edge.getFirst().getLabel().getValue());
+            Node to = nodes.get(edge.getSecond().getLabel().getValue());
+            this.addEdge(from, to);
+        }
+    }
+
+
+    public Graph(HashMap<Integer, Node> nodes, LinkedList<Edge> edges){
+        this.nodes = nodes;
+        this.edges = edges;
+    }
+
+    public HashMap<Integer, Node> getNodes() {
+        return nodes;
+    }
+    
+    public LinkedList<Edge> getEdges() {
+        return edges;
+    }
+
+    public abstract void addNode(Integer label);
+
+    public abstract void addEdge(Integer from, Integer to) throws GraphException;
+    
+    public abstract void removeEdge(Edge edge);
+
+    abstract void mergeEdge(Edge edge);
+    abstract void addEdge(Node node1, Node node2);
+
+    public abstract int minCut() throws GraphException;
 
 }
